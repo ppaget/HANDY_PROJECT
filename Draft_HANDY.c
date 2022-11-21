8bb185eb1e9331303fdad4a1e273ff744514f62e
pernoulon <pernele.paget@gmail.com>
2022-11-21 16:09:18 +0100

    ok

--- Draft_HANDY.c
+++ Draft_HANDY.c
@@ -3,7 +3,7 @@
 struct Struct_vari {
     double xc; //commoners population
     double xe; //elites population
-    double y; //nature 
+    double n; //nature 
     double w; //wealth 
 };
 
@@ -22,68 +22,54 @@ struct Struct_params {
 
 } ;
 
-struct Data {
+struct Struct_Data {
     char name;
-    double *value; // pointeur pour avoir tableaux des valeurs en fonction du temps 
+    double value; // pointeur pour avoir tableaux des valeurs en fonction du temps 
 } ;
+// pour pouvoir faire Data[i].name = Data[i].value, ie pour associer la valeur au nom 
 
-int lireFichier(char * nomFichier, struct Struct_params * tableparams, struct Struct_vari * tablevari, int longueur) { // On peut mettre le nom de struct qu'on veut ??
+
+int readFile(char * nomFichier, struct Struct_params * tableparams, struct Struct_vari * tablevari, int size) { // On peut mettre le nom de struct qu'on veut ??
     // Ouvrir le fichier
     FILE * file = fopen(nomFichier, "r"); // pointer
     if (file == NULL) return -1;  //si fichier n'existe pas
 
     // Lire ligne par ligne
     int n = 0;
+    double val;
     char line[100];
     while (fgets(line, 100, file) != NULL) { //read and store char (max 100 char) into buffer. at the end-> NULL
         // recup la valeur de la ligne strch, atof
         char * espace =strchr(line,' ');
         if (*(espace+1) != ' '){  // Si le caractère d'après n'est pas un espace, on peut transfèrer en chiffres 
-            double val = atof(espace);
+            val = atof(espace+1); // arrondir à 2 chiffres après virgule 
         }    
         else {
-            double val=atof(espace )
+            val=atof(espace+2); // arrondir à 2 chiffres après virgule 
         }
+        
         // dijonction des cas pour chaque valeur de n
-        if (n == 0) tableparams.xc = val;
+        if (n == 0) tablevari->xc = val;
+        if (n == 1) tablevari->xe = val ;
+        if (n == 2) tablevari->n = val ;
+        if (n == 3) tablevari->w = val ; // Finir pour les paramètres 
 
-
-        n = n + 1;  // if ok is 1
+        if (n == 4) 
+        n = n + 1; // if ok is 1
+        
+        if (n>size) break ; // ici, size=15
     }
     fclose(file);
-    return n;  // nombre de lignes ok pour paramètres en input 
 }
-
-int lireLigne(char * ligne, struct DataSet * data){
-
-        // Création tableau pour stocker chiffres à la fin 
-        char Data[15];
-
-        // Définition des positions respectives des 2 virgules 
-        char *tab1 = strchr(ligne, '\t');
-        char *tab2 = strchr(tab1 + 1, '\t');
-
-
-        //Condition d'invalidité de la ligne 
-        if (tab1==NULL) {
-            printf("0\nLigne non valide !\n");
-            return 0 ;
-        }
-        else {
-        
-        //Conversion de la valeur txt en nombre ; pas besoin de convertir les autres colonnes 
-        double v = atof(tab1+1) ; 
-
-        //Remplissage de la structure avec les coordonnées en chiffres 
-
-        printf("%s,%.5f\t\n", tab1, v);
-        return 1; 
-        }
-    }
 
 int main(int argc, char const *argv[]){
-    struct Struct_params params[1500] ;//= tableau de 1500 structures de types struct_params
-    struct Struct_vari vari ;
-    readfile("params_default.txt", )
+    struct Struct_vari vari[1000] ; //= tableau de 1500 structures de types struct_vari
+    struct Struct_params params ;
+
+    //option avec struct data (name+value)
+    struct Struct_Data data[10] ;
+    readFile("params_default.txt", vari, &params, 15); 
     return 0;
 }
+
+
