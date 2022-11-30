8fdff3d3fb43d285d0a217f9f9f8581afad2d507
pernoulon <pernele.paget@gmail.com>
2022-11-30 15:45:26 +0100

    modif peps

--- in_Python/anim_graph_im.py
+++ in_Python/anim_graph_im.py
@@ -7,36 +7,36 @@ from PIL import Image
 # Création de la fonction qui sera appelée à "chaque nouvelle image"
 def animate(k):
 
-    ax[0].plot(t[:k*5], XC[:k*5], color = 'b')
-    ax[0].plot(t[:k*5], XE[:k*5], color = 'r')
-    ax[0].plot(t[:k*5], N[:k*5], color = 'g')
-    ax[0].plot(t[:k*5], W[:k*5], color = 'k')
+    ax[1].plot(t[:k*5], XC[:k*5], color = 'b')
+    ax[1].plot(t[:k*5], XE[:k*5], color = 'r')
+    ax[1].plot(t[:k*5], N[:k*5], color = 'g')
+    ax[1].plot(t[:k*5], W[:k*5], color = 'k')
 
-    ax[0].legend(["Commoner population", "Elite population", "Nature", "Wealth"])
+    ax[1].legend(["Commoner population", "Elite population", "Nature", "Wealth"])
     #rendre legende plus petite
 
-    im_xc = Image.open("in Python/sample.jpeg") 
+    im_xc = Image.open("in_Python/im1.jpg") 
+    im_xe = Image.open("in_Python/im2.jpg")
+    im_n = Image.open("in_Python/im3.jpg") 
+    im_w = Image.open("in_Python/im4.jpg")  
 
-    """im_xe = Image.open('sample.jpeg') 
-    im_n = Image.open('sample.jpeg') 
-    im_w = Image.open('sample.jpeg')"""
+    ax[2].clear() #Permet d'éviter la superposition d'images de tailles diff
 
-    ax[1].clear() #Permet d'éviter la superposition d'images de tailles diff
+    ax[2].set_xlim([-1,1]) # Définit un cadre pour l'image 
+    ax[2].set_ylim([-1,1])
 
-    ax[1].set_xlim([-3,3])
-    ax[1].set_ylim([-3,3])
+    r_xc = XC[k*5]*0.5 + 0.05 # le "coeff proportionnalité" (=valeur de la col à indice k) * 2 + la valeur min
+    r_xe = XE[k*5]*0.5 + 0.05
+    r_n =   N[k*5]*0.5 + 0.05
+    r_w =   W[k*5]*0.5 + 0.05
 
-    r_xc = XC[k]*2 + 1 # le "coeff proportionnalité" (=valeur de la col à indice k) * 2 + la valeur min
-    r_xe = XE[k]*2 + 1
-    r_n = N[k]*2 + 1
-    r_w = W[k]*2 + 1
     
-    ax[1].imshow(im_xc, extent=[-r_xc, r_xc, -r_xc, r_xc]) #image en carré ; 0,5 = éloignement par rapport au centre 
-    """ ax[1].imshow(im_xe, extent=[-r_xe-0.5, r_xe-0.5, -r_xe-0.5, r_xe-0.5])
-    ax[1].imshow(im_n, extent=[-r_n+0.5, r_n+0.5, -r_n-0.5, r_n-0.5])
-    ax[1].imshow(im_w, extent=[-r_w-0.5, r_w-0.5, -r_w+0.5, r_w+0.5])"""
+    ax[2].imshow(im_xc, extent=[-r_xc-0.5, r_xc-0.5, -r_xc-0.5, r_xc-0.5]) #image en carré ; 0,5 = éloignement par rapport au centre 
+    ax[2].imshow(im_xe, extent=[-r_xe-0.5, r_xe-0.5, -r_xe+0.5, r_xe+0.5])
+    ax[2].imshow(im_n, extent=[-r_n+0.5, r_n+0.5, -r_n+0.5, r_n+0.5])
+    ax[2].imshow(im_w, extent=[-r_w+0.5, r_w+0.5, -r_w-0.5, r_w-0.5])
 
-    ax[1].axis("off")
+    ax[2].axis("off")
 
 
 
@@ -52,23 +52,25 @@ def readFile(fname):
 
 if __name__=='__main__':
 
-    time = 1000
-    fnameM = "/Users/macbookpro/Desktop/BA3/BA3-CMT/PROJECT/HANDY_PROJECT/in Python/results_python.txt"
-    fnameP = "/Users/peppa/Desktop/Ba3/CMT/PROJECT/HANDY_PROJECT/in C/results_python.txt"
+    time = 5000 # Quelle valeur du temps ? 
+    fnameM = "/Users/macbookpro/Desktop/BA3/BA3-CMT/PROJECT/HANDY_PROJECT/in_Python/results_python.txt"
+    fnameP = "/Users/peppa/Desktop/Ba3/CMT/PROJECT/HANDY_PROJECT/in_C/results_python.txt"
     [XC, XE, N, W] = readFile(fnameP)
     t = [i for i in range(time)]
     
 # Création de la figure et de l'axe
-    interface, ax = plt.subplots(1, 2, figsize=(10,5))
+    interface, ax = plt.subplots(1, 3, figsize=(14,7))
+    # Comment réduire la taille de la première colonne ?
+
 
 #Gestion des limites de la fenêtre
-    ax[0].set_xlim(-20, 1020)
-    ax[0].set_ylim(-0.03, 1.03)
+    ax[1].set_xlim(-20, 1020)
+    ax[1].set_ylim(-0.03, 1.03)
     
 
     ani = FuncAnimation(fig = interface, func = animate, frames = range(time), interval = 1, repeat = False)
 
-    
+    plt.axis("equal")
     plt.show()
 
 
--- in_Python/image.py
+++ in_Python/image.py
@@ -1,48 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation,FFMpegFileWriter
+import matplotlib.animation as ani
 from PIL import Image
-import matplotlib.image as mpimg
+
+# regarde d'abord le main avant cette fct
+def plot_dynamic(i): # pour la i eme timestep
+    # on plot à gauche sur la figure les i eme premier points de f (le sinus) 
+    ax[0].plot(x[:i], f[:i], 'r')
+
+    # maintenant l'image sur le droite de la figure
+    # on calcul la taile de l'image en fonction de la valeur de f. 
+    # abs(sinus) compris entre 0 et 1 donc quand f est max r = 0.6 et f min r=0.05
+    r = f[i]*0.55 + 0.05
+    # on clear la figure pour enlever le cercle d'avant
+    ax[1].clear()
+    # limites des axes
+    ax[1].set_xlim([-1, 1])
+    ax[1].set_ylim([-1.25, 1.25])
+    # on affiche l'image centrer en (0, 0) avec les bord de -r a r pour x et y (l'image c'est un carré)
+    ax[1].imshow(img, extent=[-r+0.5, r+0.5, -r, r])
+    ax[1].imshow(img, extent=[-r-0.5, r-0.5, -r, r])
+    # enleve les axes de gauche sinon c'est moche
+    ax[1].axis("off")
 
 
-# Création de la fonction qui sera appelée à "chaque nouvelle image"
+if __name__=='__main__':
+    # on va tracer valeur absolue de sinus entre 0 et 500 avec l'image qui grossis en fonction
+    # avec une periode de 2pi/500
+    x = np.linspace(0, 500, 100)
+    f = np.abs(np.sin(x*(2*np.pi)/(500)))
 
-img = mpimg.imread("lol.png")
+    # creation de la figure avec 2 subplot
+    # en gros on a ax = [ax0, ax1] avec ax0 les axes de gauche et ax1 axes de droite
+    # si tu veux plot sur l'axe de gauche tu fais ax[0].plot
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 3))
+    # fixe les axes de la première figure
+    ax[0].set_xlim([0, 500]) # x compris entre 0 et 500
+    ax[0].set_ylim([0, 1]) # y entre 0 et 1 -> abs(sin)
+    # plt.axis("equal") # pour avoir des subplot carré
 
-img2 = Image.open("lol.png")
-imgplot = plt.imshow(img)
-#plt.show()
-img2.show()
+    # ouvre l'image qu'on va afficher à coté 
+    img = Image.open("in_Python/im1.jpg")
 
-    #im_xc.clear() #Permet d'éviter la superposition d'images de tailles diff
-
-    # r_xc = XC[k]*2 + 1 # le "coeff proportionnalité" (=valeur de la col à indice k) * 2 + la valeur min
-    # r_xe = XE[k]*2 + 1
-    # r_n = N[k]*2 + 1
-    # r_w = W[k]*2 + 1
-    
-    # im_xc.imshow(im_xc, origin = 'lower', extent=(-0.5, r_xc, -0.5, r_xc)) #image en carré ; 0,5 = éloignement par rapport au centre 
-    # """ ax[1].imshow(im_xe, extent=[-r_xe-0.5, r_xe-0.5, -r_xe-0.5, r_xe-0.5])
-    # ax[1].imshow(im_n, extent=[-r_n+0.5, r_n+0.5, -r_n-0.5, r_n-0.5])
-    # ax[1].imshow(im_w, extent=[-r_w-0.5, r_w-0.5, -r_w+0.5, r_w+0.5])"""
-
-    # ax.axis("off")
-    # plt.show()
-
-# if __name__=='__main__':
-
-# # Création de la figure et de l'axe
-#     ax = plt.subplots(1, 2, figsize=(10,5))
-
-# #Gestion des limites de la fenêtre
-#     ax[0].set_xlim(-20, 1020)
-#     ax[0].set_ylim(-0.03, 1.03)
-
-#     ani = FuncAnimation(fig = im_xc, func = animate, frames = range(time), interval = 1, repeat = False)
-
-    
-#     plt.show()
-
-
-# enregistrer la vidéo ?
-# courbe inclusive
+    # fonction d'animation à chaque timestep elle va appeler plot dynamic avec i qui augmente
+    # interval 1 c'est pour dire que y a 1 ms entre chaque interval
+    # fig c'est pour plot sur la figure qu'on a defini au dessus
+    animator = ani.FuncAnimation(fig, plot_dynamic, interval=1)
+    plt.show()
